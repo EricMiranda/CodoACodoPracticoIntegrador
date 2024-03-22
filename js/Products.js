ed12c449296b3a8de7435a69935c6c7a7a778445
Eric Miranda <ericmiranda.rp@gmail.com>
2024-03-22 11:08:53 -0300

    Add Products img and fill function

---
+++ js/Products.js
@@ -0,0 +1,54 @@
+function DrawProducts()
+{
+    let content = document.getElementById("Content");
+    let products = GetAllProducts();
+    let elements = "";
+    products.forEach((p) => elements += "<div><img src=\""+p.Path+"\" alt=\"\"><p>"+p.Description+"</p></div>" );
+
+    content.innerHTML = elements;
+}
+
+function GetAllProducts()
+{
+    // let products = new Object[4]();
+    // let product = new Object();
+    // //Cafe 1
+    // product.Path = "../img/cafe001.png";
+    // product.Description = "Cafe Brasil";
+    // products[0] = product;
+    // //Cafe 2
+    // product.Path = "../img/cafe002.png";
+    // product.Description = "Cafe Colombia";
+    // products[0] = product;
+    // //Cafe 3
+    // product.Path = "../img/cafe003.png";
+    // product.Description = "Cafe Peru";
+    // products[0] = product;
+    // //Cafe 4
+    // product.Path = "../img/cafe004.png";
+    // product.Description = "Cafe Colombia 2";
+    // products[0] = product;
+    let products = [];
+    //Cafe 1
+    products.push({
+        Path: "../img/cafe001.png",
+        Description: "Cafe Brasil"
+    });
+    //Cafe 2
+    products.push({
+        Path: "../img/cafe002.png",
+        Description: "Cafe Colombia"
+    });
+    //Cafe 3
+    products.push({
+        Path: "../img/cafe003.png",
+        Description: "Cafe Peru"
+    });
+    //Cafe 4
+    products.push({
+        Path: "../img/cafe004.png",
+        Description: "Cafe Colombia 2"
+    });
+    
+    return products;
+}
